aea786f084a85ecea437d4f4c790d7797bb09ffe
米各馬 <migemadesign@gmail.com>
2020-12-25 15:08:43 +0800

    增加移動程式碼

---
+++ Assets/One.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class One : MonoBehaviour
+{
+    [Range(0,10)]public float speed;
+    void Start()
+    {
+        speed = speed * 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+    void movement() 
+    {
+        float moveH = Input.GetAxis("Horizontal")*speed;
+        float moveV = Input.GetAxis("Vertical")*speed;
+        Vector3 vector = new Vector3(moveH * Time.deltaTime, 0, moveV * Time.deltaTime);
+        transform.Translate(vector);
+    }
+}
